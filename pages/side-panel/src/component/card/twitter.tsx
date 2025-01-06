e01faafbaa294f71522c0b312c57dd0d84216374
alex-reinfoce <alex.lichangnan@gmail.com>
2025-01-06 20:53:23 +0800

    Enhance project dependencies and integrate Tailwind Variants for Twitter card styling
    - Added `tailwind-variants` dependency to `package.json` for improved styling capabilities.
    - Updated `pnpm-lock.yaml` to include `tailwind-variants` with its version and peer dependencies.
    - Refactored `Twitter` component in `twitter.tsx` to utilize `tailwind-variants` for dynamic styling based on the `twitterCard` prop.
    - Minor adjustments in `side-panel/package.json` for dependency organization.

--- pages/side-panel/src/component/card/twitter.tsx
+++ pages/side-panel/src/component/card/twitter.tsx
@@ -1,7 +1,29 @@
+import { tv } from 'tailwind-variants';
+
 interface TwitterProps {
   page: Page;
 }
 
+const styles = tv({
+  slots: {
+    container: 'border border-[#cfd9de] rounded-[16px]',
+  },
+  variants: {
+    twitterCard: {
+      summary: {
+        container: '',
+      },
+      summary_large_image: {
+        container: '',
+      },
+    },
+  },
+})();
+
 export const Twitter = ({ page }: TwitterProps) => {
-  return <div>{JSON.stringify(page, null, 2)}</div>;
+  if (!page.twitterCard) {
+    return null;
+  }
+
+  return <div className={styles.container({ twitterCard: page.twitterCard })}> {JSON.stringify(page, null, 2)}</div>;
 };
